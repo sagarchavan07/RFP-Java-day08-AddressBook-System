b1e6285a3ba99c456939c54caf2eb1d99b3c3842
sagar <chavansagar96044@gmail.com>
2022-07-16 23:06:36 +0530

    created new contact on address book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,5 +4,7 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
 
+        Contact contact=new Contact("Sagar","Chavan","address","Sangli","Maharashtra",415001,123456789,"sagar@exmple.com");
+        System.out.println(contact.toString());
     }
 }
