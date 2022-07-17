a7fde606f07b59667cb22b447e6cdcb20d21e3ec
sagar <chavansagar96044@gmail.com>
2022-07-17 13:38:07 +0530

    edit existing contact by name

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    static int contactID;
     static ArrayList <Contact> AddressBook=new ArrayList();
     static Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
