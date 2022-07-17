28bdf91a17d28f19196c32e96320ed0e2f9abb51
sagar <chavansagar96044@gmail.com>
2022-07-17 12:36:13 +0530

    added new contact to addressbook

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,16 +1,18 @@
 package com.bridgelabz;
 
+import java.sql.Array;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    Contact contact;
+    static int contactID;
+    static Contact AddressBook[]=new Contact[10];
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
 
         AddressBookMain addressBook=new AddressBookMain();
         Contact contact1=addressBook.createContact();
-
+        addressBook.addContact(contact1);
         System.out.println(contact1);
     }
 
@@ -33,8 +35,14 @@ public class AddressBookMain {
         System.out.println("Enter Email");
         String email=scanner.next();
 
-        System.out.println("created new contact\n");
         Contact contact=new Contact(firstName,lastName,address,city,state,zipCode,phoneNumber,email);
+        System.out.println("created new contact");
         return contact;
     }
+
+    void addContact(Contact contact){
+        AddressBook[contactID++]=contact;
+        System.out.println("contact added to AddressBook");
+    }
+
 }
