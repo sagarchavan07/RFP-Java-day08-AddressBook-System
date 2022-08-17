7761c3aac63912b639a0f376ba78d476f6145022
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-17 23:30:08 +0530

    ws

--- src/test/java/JdbcServiceTest.java
+++ src/test/java/JdbcServiceTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 public class JdbcServiceTest {
-    @Test
-    void readAddressBookMethod_should_RetrieveDataFromDatabase() throws SQLException {
-        JDBCService.readAddressBook();
-    }
-
+//    @Test
+//    void readAddressBookMethod_should_RetrieveDataFromDatabase() throws SQLException {
+//        JDBCService.readAddressBook();
+//    }
+//
 //    @Test
 //    void updateContactMethod_should_RetrieveDataFromDatabase() throws SQLException {
 //        JDBCService.updateContact("sagar");
